faf6bde75dbd8ea7c0a45ceb2398b3322bdc6584
kira_wang_aug <angelwang11a@outlook.com>
2025-01-23 16:41:03 +0800

    fixed 'is-used' property isn't displayed when unchecked: changed the chekbox to select

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, url_for
+import redis
+import base64
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -12,9 +15,21 @@ def manage():
 
 @app.route('/add_submit', methods=['POST'])
 def add_submit():
-    used = request.form.get('is-used')
-    return redirect(request.referrer)
+    #r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+    file = request.files.get('img')
+    file_content = base64.b64encode(file.read())
+    year = request.form['year']
+    country = request.form['country']
+    theme = request.form['theme']
+    form = request.form['stamp-format']
+    printing = request.form['printings']
+    descp = request.form['descrip']
+    used = request.form['is-used']
+    #img_str = base64.bsencode(img.read())
+    #print(f'img: {img_str}, year: {year}')
+    return redirect(url_for('manage'))
+
 
 
 if __name__ == '__main__': 
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
